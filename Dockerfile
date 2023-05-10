14afc47bc124b1cf85d30c45f86800b2afb98dbf
Roland Smith <rolys@zz9.cc>
2023-05-10 09:46:25 +0000

    Try to identify what kind of device we have based on ssid and manuf

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM python:3
 
-ADD kismet_exporter.py /
-
 RUN pip3 install prometheus_client
 RUN pip3 install python-dateutil
 RUN pip3 install kismet_rest
-CMD [ "python", "./kismet_exporter.py" ]
+
+COPY kismet_exporter.py /kis_exp/
+COPY pd_lookup /kis_exp/pd_lookup/
+
+CMD [ "python", "./kis_exp/kismet_exporter.py" ]
 EXPOSE 8501 
--- kismet_exporter.py
+++ kismet_exporter.py
@@ -8,7 +8,7 @@ from prometheus_client import start_http_server
 from prometheus_client.core import CounterMetricFamily,GaugeMetricFamily, REGISTRY
 import kismet_rest
 from datetime import datetime 
-
+from pd_lookup import search as pdsearch
 
 kismet_url = os.getenv('KISMET_URL',None)
 kismet_username = os.getenv('KISMET_USERNAME',None)
@@ -16,68 +16,78 @@ kismet_password = os.getenv('KISMET_PASSWORD',None)
 http_server_port = os.getenv('HTTP_PORT',8501)
 
 devices = kismet_rest.Devices(host_uri=kismet_url,username=kismet_username,password=kismet_password)
+PDlookup = pdsearch.PersonalDevice()
 
 class KisCollector(object):
 
   def __init__(self):
     self.t = int(datetime.utcnow().timestamp())
-    self.clientMap = {}
-    for device in devices.dot11_access_points():
-        bn = device['kismet.device.base.name']
-        for client in device['dot11.device']['dot11.device.associated_client_map'].items():
-            self.clientMap.setdefault(client[0],set([])).add(bn)
+    #self.clientMap = {}
+    #for device in devices.dot11_access_points():
+    #    bn = device['kismet.device.base.name']
+    #    try: 
 
+    #       if device['dot11.device']['dot11.device.num_associated_clients'] > 0:
+    #            for client in device['dot11.device']['dot11.device.associated_client_map'].items():
+    #                self.clientMap.setdefault(client[0],set([])).add(bn)
+    #    except: 
+    #        pass
 
     
   def collect(self):
     
-    smetric = GaugeMetricFamily('kismet_device_signal_strength','the signal strength of a device', labels=["mac","ssid","wifi_type","manuf"])
-    dmetric = CounterMetricFamily('kismet_device_data_seen','amount of data seen by device', labels=["mac","ssid","wifi_type","manuf"]) 
-    pmetric = CounterMetricFamily('kismet_device_packets_seen','number of packets seen',labels=["mac","ssid","wifi_type","manuf"])
-    bssmetric = GaugeMetricFamily('kismet_device_bss_timestamp','uptime in seconds of the device',labels=["mac","ssid","wifi_type","manuf"])
-    tdmetric =GaugeMetricFamily('kismet_unique_devices','number of devices seen since last query')
-    tametric =GaugeMetricFamily('kismet_unique_ap_devices','number of ap devices seen since last query')
+    smetric = GaugeMetricFamily('kismet_device_signal_strength','the signal strength of a device', labels=["mac","ssid","wifi_type","manuf","personal_device","personal_device_match"])
+    dmetric = CounterMetricFamily('kismet_device_data_seen','amount of data seen by device', labels=["mac","ssid","wifi_type","manuf","personal_device","personal_device_match"]) 
+    pmetric = CounterMetricFamily('kismet_device_packets_seen','number of packets seen',labels=["mac","ssid","wifi_type","manuf","personal_device","personal_device_match"])
+    bssmetric = GaugeMetricFamily('kismet_device_bss_timestamp','uptime in seconds of the device',labels=["mac","ssid","wifi_type","manuf","personal_device","personal_device_match"])
+    fmetric = GaugeMetricFamily('kismet_device_freq','freq of the device',labels=["mac","ssid","wifi_type","manuf","personal_device","personal_device_match"])
 
 
     devs = []
-    uniq_devices = set() #assuming mac is uniq >;/
-    uniq_aps = set() #assuming mac is uniq >;/
-    for device in devices.all(ts=self.t):
+    last = self.t
+    self.t = int(datetime.utcnow().timestamp())
+    for device in devices.all(ts=last):
         ma = device['kismet.device.base.macaddr']
-        uniq_devices.add(ma)
         devs.append(device)
         bn = device['kismet.device.base.name']
-        if device['kismet.device.base.type'] == 'Wi-Fi AP':
-            uniq_aps.add(ma)
-            for client in device['dot11.device']['dot11.device.associated_client_map'].items():
-                self.clientMap.setdefault(client[0],set([])).add(bn)
-
-    self.t = int(datetime.utcnow().timestamp())
+        #if device['kismet.device.base.type'] == 'Wi-Fi AP':
+        #    try: 
+        #        if device['dot11.device']['dot11.device.num_associated_clients'] > 0:
+        #            for client in device['dot11.device']['dot11.device.associated_client_map'].items():
+        #                self.clientMap.setdefault(client[0],set([])).add(bn)
+        #    except: 
+        #        pass
 
     for device in devs:
         ma = device['kismet.device.base.macaddr']
-        ssid = "|".join(self.clientMap.get(ma,set([device['kismet.device.base.commonname']])))
+        #ssid = "|".join(self.clientMap.get(ma,set([device['kismet.device.base.commonname']])))
+        ssid = device['kismet.device.base.commonname']
         type = device['kismet.device.base.type']
         manuf = device['kismet.device.base.manuf']
-        lastSig = device['kismet.device.base.signal']['kismet.common.signal.last_signal']
+#        pd,pdm = ["",""]`
+        pd,pdm = PDlookup.search(ssid,manuf)
+        fmetric.add_metric([ma,ssid,type,manuf,pd,pdm],device['kismet.device.base.frequency'])
+        if 'kismet.device.base.signal' in device:
+            lastSig = device['kismet.device.base.signal']['kismet.common.signal.last_signal']
+            smetric.add_metric([ma,ssid,type,manuf,pd,pdm], lastSig)
+        
         dataSize = device['kismet.device.base.datasize']
         packets = device['kismet.device.base.packets.total']
+        
+        pmetric.add_metric([ma,ssid,type,manuf,pd,pdm], packets)
+        dmetric.add_metric([ma,ssid,type,manuf,pd,pdm], dataSize)
+        
         if device['dot11.device']['dot11.device.bss_timestamp'] > 0:
             bsstimestamp = device['dot11.device']['dot11.device.bss_timestamp']/1000000
-            bssmetric.add_metric([ma,ssid,type,manuf],bsstimestamp)
-        smetric.add_metric([ma,ssid,type,manuf], lastSig)
-        pmetric.add_metric([ma,ssid,type,manuf], packets)
-        dmetric.add_metric([ma,ssid,type,manuf], dataSize)
+            bssmetric.add_metric([ma,ssid,type,manuf,pd,pdm],bsstimestamp)
         
-    tametric.add_metric([], len(uniq_aps))
-    tdmetric.add_metric([], len(uniq_devices))
+        
     
     yield smetric
     yield dmetric
     yield pmetric
     yield bssmetric
-    yield tdmetric
-    yield tametric
+    yield fmetric
     
 
 if __name__ == "__main__":
